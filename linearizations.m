aba332c5f3c19adf9430426acbd524d7c031c8f5
francis <francisjames10@gmail.com>
2016-11-15 19:49:55 -0800

    Symbolic expressions added

--- linearizations.m
+++ linearizations.m
@@ -30,11 +30,11 @@ pP3=[r*cos(ang1+ang2); r*sin(ang1+ang2);0];
 pP4=[r*cos(2*ang1+ang2); r*sin(2*ang1+ang2);0];
 pP5=[r*cos(2*ang1+2*ang2); r*sin(2*ang1+2*ang2);0];
 pP6=[r*cos(3*ang1+2*ang2); r*sin(3*ang1+2*ang2);0];
-Pall=[pP1, pP2, pP3, pP4, pP5, pP6 ,pP1];
-PallX=Pall(1,:);
-PallY=Pall(2,:);
-PallZ=Pall(3,:);
-plot3(PallX,PallY,PallZ)
+pPall=[pP1, pP2, pP3, pP4, pP5, pP6 ,pP1];
+pPallX=pPall(1,:);
+pPallY=pPall(2,:);
+pPallZ=pPall(3,:);
+plot3(pPallX,pPallY,pPallZ)
 
 B1=[r*cos(0+baseOffsetAngle); r*sin(baseOffsetAngle); -1];
 B2=[r*cos(0+baseOffsetAngle+ang2); r*sin(baseOffsetAngle+ang2); -1];
@@ -50,14 +50,26 @@ hold on;
 plot3(BallX,BallY,BallZ)
 
 for i=1:6
-    X=[Pall(1,i),Ball(1,i)];
-    Y=[Pall(2,i),Ball(2,i)];
-    Z=[Pall(3,i),Ball(3,i)];
+    X=[pPall(1,i),Ball(1,i)];
+    Y=[pPall(2,i),Ball(2,i)];
+    Z=[pPall(3,i),Ball(3,i)];
     plot3(X,Y,Z);
     hold on;
 end
 
 for i=1:6
-    Bp(i,:)=T+R*Pall(:,i);
+    BP(:,i)=T+R*pPall(:,i);
+    S(:,i)=BP(:,i)-Ball(:,i);
+    s(:,i)=S(:,i)/norm(S(:,i));
+    Mom(:,i)=cross(R*pPall(:,i),s(:,i)); %moments
 end
 
+U=[s;Mom];
+
+
+
+I=[Ixx -Ixy -Ixz; -Ixy Iyy -Iyz; -Ixz -Iyz Izz];
+Iprime=R*I*R.';
+M=[[m 0 0; 0 m 0; 0 0 m],zeros(3,3); zeros(3,3),Iprime];
+% %Add expressions for c's
+% C=[zeros(3,6); zeros(3,3), [0, c45 c46; c54 0 c56; c64 c65 0]];
